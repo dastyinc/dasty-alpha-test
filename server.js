088cefe4ab198e73864ba7ee857ebe346aee1068
03hgryan <03hgryan@gmail.com>
2022-09-29 22:18:32 -0700

    wrong process.env

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = 5000;
 
 const app = express();
 
-const server = app.listen(proccess.env.PORT || 5000, () => {
+const server = app.listen(process.env.PORT || 5000, () => {
   console.log(`server is listening on port ${PORT}`);
   console.log(`http://localhost:${PORT}`);
 });

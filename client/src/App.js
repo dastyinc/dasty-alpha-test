171ed0b94be059405955f0eded1bf1e523e0b9b1
03hgryan <03hgryan@gmail.com>
2022-09-29 22:53:51 -0700

    added test

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import './App.css';
-import {BrowserRouter as Router,
-Switch,
-Route
-} from 'react-router-dom';
-import {useEffect} from 'react';
-import {connectWithWebSocket} from './utils/wssConnection/wssConnection';
-import Dashboard from './Dashboard/Dashboard';
-import LoginPage from './LoginPage/LoginPage';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { connectWithWebSocket } from "./utils/wssConnection/wssConnection";
+import Dashboard from "./Dashboard/Dashboard";
+import Test from "./Test";
+import LoginPage from "./LoginPage/LoginPage";
 
 function App() {
   useEffect(() => {
     connectWithWebSocket();
-  },[]);
+  }, []);
   return (
     <Router>
       <Switch>
-        <Route path='/dashboard'>
-          <Dashboard/>
+        <Route path="/dashboard">
+          <Dashboard />
         </Route>
-        <Route path='/'>
-          <LoginPage/>
+        <Route path="/test">
+          <Test />
+        </Route>
+        <Route path="/">
+          <LoginPage />
         </Route>
       </Switch>
     </Router>
